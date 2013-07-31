e6b3f35c4c4f8f519eea75f37066a4432b58b036
Marvin Qian <fueledbymarvin@gmail.com>
2013-07-31 10:24:54 -0400

    Add pjax to server side

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,16 +1,31 @@
 class StaticPagesController < ApplicationController
 	def home
+		if request.headers['X-PJAX']
+		    render :layout => false
+		end
 	end
 
 	def about
+		if request.headers['X-PJAX']
+		    render :layout => false #add this option to save the time of layout rendering
+		end
 	end
 
 	def register
+		if request.headers['X-PJAX']
+		    render :layout => false #add this option to save the time of layout rendering
+		end
 	end
 
 	def schedule
+		if request.headers['X-PJAX']
+		    render :layout => false #add this option to save the time of layout rendering
+		end
 	end
 
 	def contact
+		if request.headers['X-PJAX']
+		    render :layout => false #add this option to save the time of layout rendering
+		end
 	end
 end
