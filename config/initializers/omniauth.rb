f65fd3392e24f2c9b63d684b3445b2b9aa8c5c25
Marvin Qian <fueledbymarvin@gmail.com>
2013-10-15 12:36:54 -0400

    Add production Facebook app id to config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 if Rails.env == "production"
-	ENV['fb_app_id'] = "FILL IN" 
-	ENV['fb_secret'] = "FILL IN"
+	ENV['fb_app_id'] = "153712348172227" 
+	ENV['fb_secret'] = "f15c6c55c8f07afd231df242d7a15191"
 else
 	ENV['fb_app_id'] = '343629322437383'
 	ENV['fb_secret'] = '7e510c5b72f620481bf44d5873914161'
