7ce53f9718adff3189c1cbd5e7866b33989ab5ad
Marvin Qian <fueledbymarvin@gmail.com>
2013-11-21 20:38:37 -0500

    Various content changes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -35,7 +35,7 @@ class StaticPagesController < ApplicationController
 	end
 
 	def sponsor
-		@colleges = ["Yale University", "Princeton University", "Harvard University", "Massachusetts Institute of Technology", "Columbia University", "University of Pennsylvania", "Cornell University", "Brown University", "New York University", "Carnegie Mellon University", "Johns Hopkins University", "Rutgers University", "University of Maryland", "Boston College"]
+		@colleges = ["Yale University", "Princeton University", "Harvard University", "Massachusetts Institute of Technology", "Columbia University", "University of Pennsylvania", "Cornell University", "Brown University", "New York University", "Carnegie Mellon University", "Johns Hopkins University", "Rutgers University", "University of Maryland", "Boston College", "Wellesley College"]
 		if request.headers['X-PJAX']
 		    render :layout => false #add this option to save the time of layout rendering
 		end
